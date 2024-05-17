3cd7339085099056384219869153c22acec24e98
Manuel Borrero Guerrero <manubg32@gmail.com>
2024-05-17 13:19:40 +0200

    Cambio de "Hola Mundo" a "HelloWorld"

---
+++ helloWorld/src/unico/HelloWorld.java
@@ -0,0 +1,12 @@
+package unico;
+
+public class HelloWorld
+{
+ // Tu programa comienza con una llamada a main().
+ // Imprime "Hola Mundo" a la ventana de la terminal.
+ public static void main(String args[])
+ {
+ System.out.println("HelloWorld");
+ }
+}
+
