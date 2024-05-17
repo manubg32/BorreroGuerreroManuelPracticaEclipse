155f4a9fc7dff7522f1a498bab14f93ff80497e0
manubg32 <169088437+manubg32@users.noreply.github.com>
2024-05-17 13:51:23 +0200

    Update HelloWorld.java

--- helloWorld/src/unico/HelloWorld.java
+++ helloWorld/src/unico/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
  // Imprime "Hola Mundo" a la ventana de la terminal.
  public static void main(String args[])
  {
- System.out.println("HelloWorld");
+ System.out.println("Hola Mundo desde GitHub");
  }
 }
 
